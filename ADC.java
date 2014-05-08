4148b4eee753f1e784abc128a9f84e8af2f618d2
markd315 <markd315@gmail.com>
2014-05-08 01:18:56 -0400

    Create ADC.java

---
+++ ADC.java
@@ -0,0 +1,9 @@
+
+public class ADC extends Champion
+{
+
+	public ADC(String n, Champion[] cp, Champion[] s, int atk, int mgc, int hp, int crc, int mob, String[] al)
+	{
+		super(n, cp, s, atk, mgc, hp, crc, mob, al);
+	}
+}
