00be2b39f7e192aecbdaceb17b75a57618639ebc
markd315 <markd315@gmail.com>
2014-05-08 01:20:29 -0400

    Create Support.java

---
+++ Support.java
@@ -0,0 +1,9 @@
+
+public class Support extends Champion
+{
+
+	public Support(String n, Champion[] cp, Champion[] s, int atk, int mgc, int hp, int crc, int mob, String[] al)
+	{
+		super(n, cp, s, atk, mgc, hp, crc, mob, al);
+	}
+}
